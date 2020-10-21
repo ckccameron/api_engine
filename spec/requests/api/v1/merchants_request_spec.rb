679a027d4f1719cf7d18415a79a9b9d257fc262a
Cam C <ckc.cameron@gmail.com>
2020-10-20 18:23:23 -0700

    Write spec for merchants show request

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -9,6 +9,7 @@ describe "Merchants API" do
     expect(response).to be_successful
 
     body = JSON.parse(response.body, symbolize_names: true)
+
     expect(body[:data].count).to eq(3)
     expect(body[:data].first).to have_key(:id)
     expect(body[:data].first).to have_key(:type)
@@ -17,5 +18,24 @@ describe "Merchants API" do
     expect(body[:data].first[:attributes][:id]).to be_an(Integer)
     expect(body[:data].first[:attributes]).to have_key(:name)
     expect(body[:data].first[:attributes][:name]).to be_a(String)
+  end
+
+  it "sends merchant based on unique id" do
+    merchant = create(:merchant)
+    id = merchant.id
+
+    get "/api/v1/merchants/#{id}"
+
+    expect(response).to be_successful
+
+    body = JSON.parse(response.body, symbolize_names: true)
+    
+    expect(body[:data]).to have_key(:id)
+    expect(body[:data]).to have_key(:type)
+    expect(body[:data]).to have_key(:attributes)
+    expect(body[:data][:id]).to eq(id.to_s)
+    expect(body[:data][:type]).to eq("merchant")
+    expect(body[:data][:attributes][:id]).to eq(id)
+    expect(body[:data][:attributes][:name]).to eq(merchant.name)
   end
 end

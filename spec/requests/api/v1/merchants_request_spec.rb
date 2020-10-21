be4e75e2a4a5b11738e6e50985126a010d3e8a8c
Cam C <ckc.cameron@gmail.com>
2020-10-20 18:30:08 -0700

    Write spec for merchants create/post request

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -29,13 +29,29 @@ describe "Merchants API" do
     expect(response).to be_successful
 
     body = JSON.parse(response.body, symbolize_names: true)
-    
+
     expect(body[:data]).to have_key(:id)
     expect(body[:data]).to have_key(:type)
     expect(body[:data]).to have_key(:attributes)
     expect(body[:data][:id]).to eq(id.to_s)
     expect(body[:data][:type]).to eq("merchant")
     expect(body[:data][:attributes][:id]).to eq(id)
+    expect(body[:data][:attributes][:name]).to eq(merchant.name)
+  end
+
+  it "allows for a new merchant to be created" do
+    post "/api/v1/merchants", params: {name: "Royal Palace of Gold"}
+
+    expect(response).to be_successful
+
+    merchant = Merchant.last
+
+    body = JSON.parse(response.body, symbolize_names: true)
+    
+    expect(body[:data]).to have_key(:id)
+    expect(body[:data]).to have_key(:type)
+    expect(body[:data]).to have_key(:attributes)
+    expect(body[:data][:attributes][:id]).to eq(merchant.id)
     expect(body[:data][:attributes][:name]).to eq(merchant.name)
   end
 end

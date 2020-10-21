2a4cfde99ad0cf4906f743220a870404beffecd0
Cam C <ckc.cameron@gmail.com>
2020-10-20 18:11:15 -0700

    Add response body expectations to merchants index request spec

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -13,5 +13,9 @@ describe "Merchants API" do
     expect(body[:data].first).to have_key(:id)
     expect(body[:data].first).to have_key(:type)
     expect(body[:data].first).to have_key(:attributes)
+    expect(body[:data].first[:attributes]).to have_key(:id)
+    expect(body[:data].first[:attributes][:id]).to be_an(Integer)
+    expect(body[:data].first[:attributes]).to have_key(:name)
+    expect(body[:data].first[:attributes][:name]).to be_a(String)
   end
 end

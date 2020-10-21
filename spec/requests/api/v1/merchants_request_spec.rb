750f07ab6a389344ff168773b6ab3bc3c07e35df
Cam C <ckc.cameron@gmail.com>
2020-10-20 18:07:00 -0700

    Adjust request body to symbolize names

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -8,10 +8,10 @@ describe "Merchants API" do
 
     expect(response).to be_successful
 
-    body = JSON.parse(response.body)
-    expect(body["data"].count).to eq(3)
-    expect(body["data"].first).to have_key(:id)
-    expect(body["data"].first).to have_key(:type)
-    expect(body["data"].first).to have_key(:attributes)
+    body = JSON.parse(response.body, symbolize_names: true)
+    expect(body[:data].count).to eq(3)
+    expect(body[:data].first).to have_key(:id)
+    expect(body[:data].first).to have_key(:type)
+    expect(body[:data].first).to have_key(:attributes)
   end
 end

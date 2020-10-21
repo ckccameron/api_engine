ee1374eeda2cfae6256c01063aa4a082ac818f4b
Cam C <ckc.cameron@gmail.com>
2020-10-20 19:53:46 -0700

    Add structure and key expectations for merchants items request spec

--- spec/requests/api/v1/merchant_relationships_request_spec.rb
+++ spec/requests/api/v1/merchant_relationships_request_spec.rb
@@ -20,15 +20,23 @@ describe "Merchant relationship to Item request" do
     id1_items = JSON.parse(response.body, symbolize_names: true)
 
     expect(id1_items[:data].count).to eq(3)
-    expect(id1_items[:data].first[:id]).to eq(first_item.id.to_s)
-    expect(id1_items[:data].last[:id]).to_not eq(last_item.id.to_s)
-
-    get "/api/v1/merchants/#{id2}/items"
-
-    expect(response).to be_successful
-
-    id2_items = JSON.parse(response.body, symbolize_names: true)
-    expect(id2_items[:data].count).to eq(1)
-    expect(id2_items[:data].first[:id]).to eq(last_item.id.to_s)
+    expect(id1_items[:data].first).to have_key(:id)
+    expect(id1_items[:data].first).to have_key(:type)
+    expect(id1_items[:data].first).to have_key(:attributes)
+    expect(id1_items[:data].first[:attributes]).to have_key(:id)
+    expect(id1_items[:data].first[:attributes][:id]).to be_an(Integer)
+    expect(id1_items[:data].first[:attributes][:id]).to_not eq(nil)
+    expect(id1_items[:data].first[:attributes]).to have_key(:name)
+    expect(id1_items[:data].first[:attributes][:name]).to be_a(String)
+    expect(id1_items[:data].first[:attributes][:name]).to_not eq(nil)
+    expect(id1_items[:data].first[:attributes]).to have_key(:description)
+    expect(id1_items[:data].first[:attributes][:description]).to be_a(String)
+    expect(id1_items[:data].first[:attributes][:description]).to_not eq(nil)
+    expect(id1_items[:data].first[:attributes]).to have_key(:unit_price)
+    expect(id1_items[:data].first[:attributes][:unit_price]).to be_a(Float)
+    expect(id1_items[:data].first[:attributes][:unit_price]).to_not eq(nil)
+    expect(id1_items[:data].first[:attributes]).to have_key(:merchant_id)
+    expect(id1_items[:data].first[:attributes][:merchant_id]).to be_an(Integer)
+    expect(id1_items[:data].first[:attributes][:merchant_id]).to_not eq(nil)
   end
 end

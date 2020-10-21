7695999d1d4505c04f918467db7b976517c513e9
Cam C <ckc.cameron@gmail.com>
2020-10-21 00:45:06 -0700

    Add expectations to merchants single finder spec
    * test that it works for name attribute as well as created_at and 
    updated_at
    *

--- spec/requests/api/v1/merchants_finders_spec.rb
+++ spec/requests/api/v1/merchants_finders_spec.rb
@@ -3,17 +3,68 @@ require 'rails_helper'
 describe "Merchants finder requests" do
   it "can return single merchant when given query params" do
     create(:merchant, name: "Basketball Blvd")
-    create(:merchant, name: "Basketball Bonanza")
-    create(:merchant, name: "Full Court Press")
 
-    get "/api/v1/merchants/find?name=basketball"
+    attribute = "name"
+    query = "bAsKeTbaLl"
+
+    get "/api/v1/merchants/find?#{attribute}=#{query}"
 
     expect(response).to be_successful
 
     merchant_result = JSON.parse(response.body, symbolize_names: true)
-    name = search_body[:data][:attributes][:name].downcase
+    name = merchant_result[:data][:attributes][:name].downcase
 
-    expect(search_body[:data]).to be_a(Hash)
-    expect(name).to include("in")
+    expect(merchant_result[:data]).to be_a(Hash)
+    expect(merchant_result[:data]).to have_key(:id)
+    expect(merchant_result[:data]).to have_key(:type)
+    expect(merchant_result[:data]).to have_key(:attributes)
+    expect(merchant_result[:data][:type]).to eq("merchant")
+    expect(merchant_result[:data][:attributes]).to have_key(:id)
+    expect(merchant_result[:data][:attributes][:id]).to be_an(Integer)
+    expect(merchant_result[:data][:attributes]).to have_key(:name)
+    expect(merchant_result[:data][:attributes][:name]).to be_an(String)
+    expect(name).to include(query.downcase)
+  end
+
+  it "can return single merchant when given created at or updated at query" do
+    create(:merchant, name: "Basketball Blvd", created_at: "'Wed, 21 Oct 2020 12:00:00 UTC +00:00'", updated_at: "'Wed, 21 Oct 2020 12:00:00 UTC +00:00'")
+
+    attribute1 = "created_at"
+    query1 = "21"
+
+    get "/api/v1/merchants/find?#{attribute1}=#{query1}"
+
+    expect(response).to be_successful
+
+    merchant_result = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchant_result[:data]).to be_a(Hash)
+    expect(merchant_result[:data]).to have_key(:id)
+    expect(merchant_result[:data]).to have_key(:type)
+    expect(merchant_result[:data]).to have_key(:attributes)
+    expect(merchant_result[:data][:type]).to eq("merchant")
+    expect(merchant_result[:data][:attributes]).to have_key(:id)
+    expect(merchant_result[:data][:attributes][:id]).to be_an(Integer)
+    expect(merchant_result[:data][:attributes]).to have_key(:name)
+    expect(merchant_result[:data][:attributes][:name]).to be_an(String)
+
+    attribute2 = "updated_at"
+    query2 = "Oct"
+
+    get "/api/v1/merchants/find?#{attribute2}=#{query2}"
+
+    expect(response).to be_successful
+
+    merchant_result = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchant_result[:data]).to be_a(Hash)
+    expect(merchant_result[:data]).to have_key(:id)
+    expect(merchant_result[:data]).to have_key(:type)
+    expect(merchant_result[:data]).to have_key(:attributes)
+    expect(merchant_result[:data][:type]).to eq("merchant")
+    expect(merchant_result[:data][:attributes]).to have_key(:id)
+    expect(merchant_result[:data][:attributes][:id]).to be_an(Integer)
+    expect(merchant_result[:data][:attributes]).to have_key(:name)
+    expect(merchant_result[:data][:attributes][:name]).to be_an(String)
   end
 end

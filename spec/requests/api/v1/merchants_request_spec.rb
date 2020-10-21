e7931517ccd61d12d0d6989d64bb4945ac35faac
Cam C <ckc.cameron@gmail.com>
2020-10-20 19:17:01 -0700

    Write spec for merchants destroy request

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -39,7 +39,7 @@ describe "Merchants API" do
     expect(body[:data][:attributes][:name]).to eq(merchant.name)
   end
 
-  it "allows for a new merchant to be created" do
+  it "allows a new merchant to be created" do
     post "/api/v1/merchants", params: {name: "Royal Palace of Gold"}
 
     expect(response).to be_successful
@@ -55,7 +55,7 @@ describe "Merchants API" do
     expect(body[:data][:attributes][:name]).to eq(merchant.name)
   end
 
-  it "allows for a merchant to be updated" do
+  it "allows a merchant to be updated" do
     merchant = create(:merchant)
     id = merchant.id
     old_name = Merchant.last.name
@@ -65,12 +65,26 @@ describe "Merchants API" do
     expect(response).to be_successful
 
     body = JSON.parse(response.body, symbolize_names: true)
-    
+
     expect(body[:data]).to have_key(:id)
     expect(body[:data]).to have_key(:type)
     expect(body[:data]).to have_key(:attributes)
     expect(body[:data][:attributes][:id]).to eq(merchant.id)
     expect(body[:data][:attributes][:name]).to eq("It's A New Day")
     expect(body[:data][:attributes][:name]).to_not eq(old_name)
+  end
+
+  it "allows a merchant to be destroyed" do
+    id1 = create(:merchant).id
+    id2 = create(:merchant).id
+
+    expect(Merchant.count).to eq(2)
+
+    delete "/api/v1/merchants/#{id1}"
+
+    expect(response.status).to eq(204)
+
+    expect(Merchant.count).to eq(1)
+    expect{ Item.find(id1) }.to raise_error(ActiveRecord::RecordNotFound)
   end
 end

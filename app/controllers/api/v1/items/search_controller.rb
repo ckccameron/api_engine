11627e8bfc6bc2d67cf3e536215b13c048aaeb80
Cam C <ckc.cameron@gmail.com>
2020-10-21 22:35:09 -0700

    Add empty index action to items search controller

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -1,4 +1,8 @@
 class Api::V1::Items::SearchController < ApplicationController
+  def index
+
+  end
+
   def show
     render json: ItemSerializer.new(Item.find_single_item(search_params))
   end
@@ -6,6 +10,6 @@ class Api::V1::Items::SearchController < ApplicationController
   private
 
   def search_params
-    params.permit(:name, :description, :unit_price, :merchant_id)
+    params.permit(:name, :description, :unit_price, :merchant_id, :created_at, :updated_at)
   end
 end

deaa40abaac5140e620040dac2b9a854866f4d33
Cam C <ckc.cameron@gmail.com>
2020-10-21 22:54:18 -0700

    Write spec for items multi-finder using created_at or updated_at as search param

--- spec/requests/api/v1/items_finders_spec.rb
+++ spec/requests/api/v1/items_finders_spec.rb
@@ -129,4 +129,67 @@ describe "Items finder requests" do
       expect(name).to include(query)
     end
   end
+
+  it "can return all items when given created at or updated at query" do
+    merchant = create(:merchant)
+    item1 = create(:item, name: "Boomshakalaka", merchant_id: merchant.id, created_at: "21 Oct 2020 12:00:00 UTC +00:00", updated_at: "21 Oct 2020 12:00:00 UTC +00:00")
+    item2 = create(:item, name: "Kaboom", merchant_id: merchant.id, created_at: "21 Oct 2020 12:00:00 UTC +00:00", updated_at: "21 Oct 2020 12:00:00 UTC +00:00")
+    item3 = create(:item, name: "Boom Boom Bass Speaker", merchant_id: merchant.id, created_at: "21 Aug 2020 12:00:00 UTC +00:00", updated_at: "21 Aug 2020 12:00:00 UTC +00:00")
+
+    attribute1 = "created_at"
+    query1 = "21"
+
+    get "/api/v1/items/find_all?#{attribute1}=#{query1}"
+
+    expect(response).to be_successful
+
+    items_results = JSON.parse(response.body, symbolize_names: true)
+
+    expect(items_results[:data].count).to eq(3)
+    expect(items_results[:data]).to be_an(Array)
+    items_results[:data].each do |item|
+      expect(item).to have_key(:id)
+      expect(item).to have_key(:type)
+      expect(item).to have_key(:attributes)
+      expect(item[:type]).to eq("item")
+      expect(item[:attributes]).to have_key(:id)
+      expect(item[:attributes][:id]).to be_an(Integer)
+      expect(item[:attributes]).to have_key(:name)
+      expect(item[:attributes][:name]).to be_a(String)
+      expect(item[:attributes]).to have_key(:description)
+      expect(item[:attributes][:description]).to be_a(String)
+      expect(item[:attributes]).to have_key(:unit_price)
+      expect(item[:attributes][:unit_price]).to be_a(Float)
+      expect(item[:attributes]).to have_key(:merchant_id)
+      expect(item[:attributes][:merchant_id]).to be_an(Integer)
+    end
+
+    attribute2 = "updated_at"
+    query2 = "Oct"
+
+    get "/api/v1/items/find_all?#{attribute2}=#{query2}"
+
+    expect(response).to be_successful
+
+    items_results = JSON.parse(response.body, symbolize_names: true)
+
+    expect(items_results[:data].count).to eq(2)
+    expect(items_results[:data]).to be_an(Array)
+    items_results[:data].each do |item|
+      expect(item).to have_key(:id)
+      expect(item).to have_key(:type)
+      expect(item).to have_key(:attributes)
+      expect(item[:type]).to eq("item")
+      expect(item[:attributes]).to have_key(:id)
+      expect(item[:attributes][:id]).to be_an(Integer)
+      expect(item[:attributes]).to have_key(:name)
+      expect(item[:attributes][:name]).to be_a(String)
+      expect(item[:attributes]).to have_key(:description)
+      expect(item[:attributes][:description]).to be_a(String)
+      expect(item[:attributes]).to have_key(:unit_price)
+      expect(item[:attributes][:unit_price]).to be_a(Float)
+      expect(item[:attributes]).to have_key(:merchant_id)
+      expect(item[:attributes][:merchant_id]).to be_an(Integer)
+    end
+  end
 end

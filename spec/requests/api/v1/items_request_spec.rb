bc8a878ce6c5b4c246a02daacd4912b050c306b7
Cam C <ckc.cameron@gmail.com>
2020-10-21 20:21:13 -0700

    Add test expectations for item attributes beyond id and name

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -18,10 +18,19 @@ describe "Item requests" do
     expect(body[:data].count).to eq(3)
     expect(body[:data].first).to have_key(:id)
     expect(body[:data].first).to have_key(:type)
+    body[:data].each do |item|
+      expect(item[:type]).to eq("item")
+    end
     expect(body[:data].first).to have_key(:attributes)
     expect(body[:data].first[:attributes]).to have_key(:id)
     expect(body[:data].first[:attributes][:id]).to be_an(Integer)
     expect(body[:data].first[:attributes]).to have_key(:name)
     expect(body[:data].first[:attributes][:name]).to be_a(String)
+    expect(body[:data].first[:attributes]).to have_key(:description)
+    expect(body[:data].first[:attributes][:description]).to be_a(String)
+    expect(body[:data].first[:attributes]).to have_key(:unit_price)
+    expect(body[:data].first[:attributes][:unit_price]).to be_a(Float)
+    expect(body[:data].first[:attributes]).to have_key(:merchant_id)
+    expect(body[:data].first[:attributes][:merchant_id]).to be_an(Integer)
   end
 end

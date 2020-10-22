b064436d8dbf47ecb81dd1a95f68c2de542e9187
Cam C <ckc.cameron@gmail.com>
2020-10-21 22:34:52 -0700

    Fix typo in scopes for Item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,8 +3,8 @@ class Item < ApplicationRecord
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
 
-  scope :date_search, -> (param) { where("to_char(#{param.keys.first}, 'yyyy-mon-dd-HH-MI-SS') ILIKE ?", "%#{param.values.first}%") }
-  scope :attribute_search, -> (param) { where("items.#{param.keys.first}::text ILIKE ?", "%#{param.values.first}%") }
+  scope :date_search, -> (param) {where("to_char(#{param.keys.first},'yyyy-mon-dd-HH-MI-SS') ILIKE ?", "%#{param.values.first}%")}
+  scope :attribute_search, -> (param) {where("items.#{param.keys.first}::text ILIKE ?", "%#{param.values.first}%")}
 
   def self.find_single_item(param)
     attribute = param.keys.first

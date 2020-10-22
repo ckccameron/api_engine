62498a1d48e277ab2cb36efe9e2c0ea6e383b1e2
Cam C <ckc.cameron@gmail.com>
2020-10-21 22:02:26 -0700

    Create find_single_item method for Item model
    * implement scopes for date (created_at, updated_at) and attribute 
    (name) to find item based on type of query passed through

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,16 @@ class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
+
+  scope :date_search, -> (param) { where("to_char(#{param.keys.first}, 'yyyy-mon-dd-HH-MI-SS') ILIKE ?", "%#{param.values.first}%") }
+  scope :attribute_search, -> (param) { where("items.#{param.keys.first}::text ILIKE ?", "%#{param.values.first}%") }
+
+  def self.find_single_item(param)
+    attribute = param.keys.first
+    if attribute == 'created_at' || attribute == 'updated_at'
+      date_search(param).first
+    else
+      attribute_search(param).first
+    end
+  end
 end

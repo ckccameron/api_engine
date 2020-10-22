474d14a5bfe612b78878c8d2a1e2f8c2186b6799
Cam C <ckc.cameron@gmail.com>
2020-10-21 20:09:28 -0700

    Write initial spec for items index request

---
+++ spec/requests/api/v1/items_request_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe "Item requests" do
+  it "sends index of items" do
+    create :merchant
+    merchant = Merchant.last
+
+    create(:item, merchant_id: merchant.id)
+    create(:item, merchant_id: merchant.id)
+    create(:item, merchant_id: merchant.id)
+
+    get "/api/v1/items"
+
+    expect(response).to be_successful
+
+    body = JSON.parse(response.body, symbolize_names: true)
+
+    expect(body[:data].count).to eq(3)
+    expect(body[:data].first).to have_key(:id)
+    expect(body[:data].first).to have_key(:type)
+    expect(body[:data].first).to have_key(:attributes)
+    expect(body[:data].first[:attributes]).to have_key(:id)
+    expect(body[:data].first[:attributes][:id]).to be_an(Integer)
+    expect(body[:data].first[:attributes]).to have_key(:name)
+    expect(body[:data].first[:attributes][:name]).to be_a(String)
+  end
+end

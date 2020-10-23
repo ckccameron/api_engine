7b8dc313aadc51d866fc7128b1078aebff84d7e4
Cam C <ckc.cameron@gmail.com>
2020-10-23 07:25:15 -0700

    Add test for Merchant model most_revenue class method

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -43,5 +43,40 @@ describe Merchant, type: :model do
       expect(Merchant.find_all_merchants(param)).to eq([merchant1, merchant2])
       expect(Merchant.find_all_merchants(param)).to_not include(merchant3)
     end
+
+    it ".most_revenue(quantity)" do
+      customer = create(:customer)
+
+      merchant1 = create(:merchant)
+      merchant2 = create(:merchant)
+      merchant3 = create(:merchant)
+
+      item1 = create(:item, merchant: merchant1)
+      item2 = create(:item, merchant: merchant2)
+      item3 = create(:item, merchant: merchant3)
+
+      invoice1 = Invoice.create(customer: customer, merchant: merchant1, status: "shipped")
+      invoice2 = Invoice.create(customer: customer, merchant: merchant2, status: "shipped")
+      invoice3 = Invoice.create(customer: customer, merchant: merchant2, status: "shipped")
+      invoice4 = Invoice.create(customer: customer, merchant: merchant3, status: "shipped")
+      invoice5 = Invoice.create(customer: customer, merchant: merchant3, status: "shipped")
+
+      item_invoice1 = InvoiceItem.create(item: item1, invoice: invoice1, quantity: 15, unit_price: item1.unit_price)
+      item_invoice2 = InvoiceItem.create(item: item2, invoice: invoice2, quantity: 7, unit_price: item2.unit_price)
+      item_invoice3 = InvoiceItem.create(item: item2, invoice: invoice3, quantity: 5, unit_price: item2.unit_price)
+      item_invoice4 = InvoiceItem.create(item: item3, invoice: invoice4, quantity: 8, unit_price: item3.unit_price)
+      item_invoice5 = InvoiceItem.create(item: item3, invoice: invoice5, quantity: 4, unit_price: item3.unit_price)
+
+      transaction1 = Transaction.create(invoice: invoice1, credit_card_number: '111111111', credit_card_expiration_date: nil, result: "success")
+      transaction2 = Transaction.create(invoice: invoice2, credit_card_number: '222222222', credit_card_expiration_date: nil, result: "success")
+      transaction3 = Transaction.create(invoice: invoice3, credit_card_number: '111111111', credit_card_expiration_date: nil, result: "success")
+      transaction4 = Transaction.create(invoice: invoice4, credit_card_number: '222222222', credit_card_expiration_date: nil, result: "success")
+      transaction5 = Transaction.create(invoice: invoice5, credit_card_number: '111111111', credit_card_expiration_date: nil, result: "success")
+
+      quantity = 2
+
+      expect(Merchant.most_revenue(quantity)).to eq([merchant1, merchant2])
+      expect(Merchant.most_revenue(quantity)).to_not include(merchant3)
+    end
   end
 end

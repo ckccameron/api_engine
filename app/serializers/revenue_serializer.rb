86c539f3d189b99e06cc6ca9ce9d2dbb95696d4d
Cam C <ckc.cameron@gmail.com>
2020-10-22 22:16:35 -0700

    Cleanup revenue PORO and serializer
    * delete commented out code
    * fix spacing

--- app/serializers/revenue_serializer.rb
+++ app/serializers/revenue_serializer.rb
@@ -1,14 +1,4 @@
 class RevenueSerializer
   include FastJsonapi::ObjectSerializer
   attributes :id, :revenue
-  # def self.revenue(revenue)
-  #   {
-  #     "data": {
-  #       "id": nil,
-  #       "attributes": {
-  #         "revenue": revenue
-  #       }
-  #     }
-  #   }
-  # end
 end

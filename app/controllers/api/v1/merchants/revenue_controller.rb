756439d624f3ad2dbe69238091b985baa92dd4de
Cam C <ckc.cameron@gmail.com>
2020-10-22 19:38:26 -0700

    Dreamdrive show action for merchants revenue controller

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -3,4 +3,10 @@ class Api::V1::Merchants::RevenueController < ApplicationController
     quantity = params[:quantity]
     render json: MerchantSerializer.new(Merchant.most_revenue(quantity))
   end
+
+  def show
+    id = params[:merchant_id]
+    value = Merchant.revenue(id)
+    render json: RevenueSerializer.new(Revenue.new(value))
+  end
 end

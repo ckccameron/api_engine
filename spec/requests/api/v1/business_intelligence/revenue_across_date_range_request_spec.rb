57097145e34ae34c62d2b0efffb1dbc5f1b2d506
Cam C <ckc.cameron@gmail.com>
2020-10-23 06:54:10 -0700

    Add created_at to invoices to set date ranges for tests

--- spec/requests/api/v1/business_intelligence/revenue_across_date_range_request_spec.rb
+++ spec/requests/api/v1/business_intelligence/revenue_across_date_range_request_spec.rb
@@ -13,12 +13,12 @@ describe "business intelligence requests" do
     item2 = create(:item, merchant: merchant2)
     item3 = create(:item, merchant: merchant3)
 
-    invoice1 = Invoice.create(customer: customer1, merchant: merchant1, status: "shipped")
-    invoice2 = Invoice.create(customer: customer1, merchant: merchant2, status: "shipped")
-    invoice3 = Invoice.create(customer: customer1, merchant: merchant3, status: "shipped")
-    invoice4 = Invoice.create(customer: customer2, merchant: merchant1, status: "shipped")
-    invoice5 = Invoice.create(customer: customer2, merchant: merchant2, status: "shipped")
-    invoice6 = Invoice.create(customer: customer2, merchant: merchant3, status: "shipped")
+    invoice1 = Invoice.create(customer: customer1, merchant: merchant1, status: "shipped", created_at: 4.days.ago)
+    invoice2 = Invoice.create(customer: customer1, merchant: merchant2, status: "shipped", created_at: 4.days.ago)
+    invoice3 = Invoice.create(customer: customer1, merchant: merchant3, status: "shipped", created_at: 3.days.ago)
+    invoice4 = Invoice.create(customer: customer2, merchant: merchant1, status: "shipped", created_at: 2.days.ago)
+    invoice5 = Invoice.create(customer: customer2, merchant: merchant2, status: "shipped", created_at: 2.days.ago)
+    invoice6 = Invoice.create(customer: customer2, merchant: merchant3, status: "shipped", created_at: 1.days.ago)
 
     item_invoice1 = InvoiceItem.create(item: item1, invoice: invoice1, quantity: 1, unit_price: item1.unit_price)
     item_invoice2 = InvoiceItem.create(item: item2, invoice: invoice2, quantity: 1, unit_price: item2.unit_price)
@@ -34,19 +34,19 @@ describe "business intelligence requests" do
     transaction5 = Transaction.create(invoice: invoice5, credit_card_number: '111111111', credit_card_expiration_date: nil, result: "success")
     transaction6 = Transaction.create(invoice: invoice6, credit_card_number: '222222222', credit_card_expiration_date: nil, result: "success")
 
-    start_date = "2020-03-10"
-    end_date = "2020-03-21"
-
+    start_date = 3.days.ago
+    end_date = Date.today
     get "/api/v1/revenue?start=#{start_date}&end=#{end_date}"
 
     expect(response).to be_successful
 
     result = JSON.parse(response.body, symbolize_names: true)
-    binding.pry
+
     expect(result).to be_a(Hash)
     expect(result[:data]).to have_key(:id)
     expect(result[:data][:id]).to eq(nil)
     expect(result[:data]).to have_key(:attributes)
     expect(result[:data][:attributes]).to have_key(:revenue)
+    expect(result[:data][:attributes][:revenue]).to be_a(Float)
   end
 end

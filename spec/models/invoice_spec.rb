a78a25a6adca9d6a842017b6ccf126467a93f87a
Cam C <ckc.cameron@gmail.com>
2020-10-23 07:13:21 -0700

    Remove commented out code to cleanup files

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -30,7 +30,6 @@ describe Invoice, type: :model do
     it { should belong_to(:customer) }
     it { should belong_to(:merchant) }
     it { should have_many(:invoice_items) }
-    # it { should have_many(:items).through(:invoice_items) }
     it { should have_many(:transactions) }
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -32,7 +32,6 @@ describe Item, type: :model do
   describe "relationships" do
     it { should belong_to(:merchant) }
     it { should have_many(:invoice_items) }
-    # it { should have_many(:invoices).through(:invoice_items) }
   end
 
   describe "class methods" do
